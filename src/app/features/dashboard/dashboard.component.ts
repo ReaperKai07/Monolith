9bab4f7dd9c78bb2a68afd8eb078dd76fc8cead3
Khairul Izzat Roslan <69512217+ReaperKai07@users.noreply.github.com>
2026-07-18 19:36:34 +0800

    create dashboard

---
+++ src/app/features/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [
+    MatIconModule
+  ],
+  templateUrl: './dashboard.component.html',
+})
+
+export class DashboardComponent {
+
+}
