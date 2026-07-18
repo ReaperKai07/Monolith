0324372cf22b72d33fe301f14e76497be05f463b
Khairul Izzat Roslan <69512217+ReaperKai07@users.noreply.github.com>
2026-07-18 19:35:12 +0800

    fix tailwind and code cleaning

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children:[
+      {
+        path:'',
+        component:DashboardComponent
+      }
+    ]
+  }
+
+];
