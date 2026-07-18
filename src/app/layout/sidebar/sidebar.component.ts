172b7533b052c1f1aa33dbe3820c8a906018d874
Khairul Izzat Roslan <69512217+ReaperKai07@users.noreply.github.com>
2026-07-18 19:36:13 +0800

    create header, side-bar, and main-layout

---
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [
+
+  ],
+  templateUrl: './sidebar.component.html',
+})
+
+export class SidebarComponent {
+
+}
