e68af00f6ad455fb705cf4151bdc433cbb95f17f
Khairul Izzat Roslan <69512217+ReaperKai07@users.noreply.github.com>
2026-07-19 19:43:31 +0800

    fix routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,38 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { DashboardComponent } from './modules/admin/dashboard/dashboard.component';
-
+import { SignInComponent } from './modules/auth/sign-in/sign-in.component';
 
 export const routes: Routes = [
 
+  // Redirect empty path
+  {
+    path: '',
+    redirectTo: 'sign-in',
+    pathMatch: 'full'
+  },
+
+  // Redirect signed-in user
+  { 
+    path: 'signed-in-redirect', 
+    pathMatch: 'full', 
+    redirectTo: 'dashboard' 
+  },
+
+  // Auth routes for authenticated users
+  {
+    path: 'sign-in',
+    component: SignInComponent
+  },
+
+  // Main application
   {
     path: '',
     component: MainLayoutComponent,
-    children:[
+    children: [
       {
-        path:'',
-        component:DashboardComponent
+        path: 'dashboard',
+        component: DashboardComponent
       }
     ]
   }
