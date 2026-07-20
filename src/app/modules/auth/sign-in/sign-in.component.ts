13778f5c9619ddfbf5677af7c9f940bb10447cd8
Khairul Izzat Roslan <69512217+ReaperKai07@users.noreply.github.com>
2026-07-20 22:48:09 +0800

    call getUserDetails in signIn, and code cleaning

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../../core/auth/auth.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -10,52 +10,84 @@ import { CommonModule } from '@angular/common';
 import { LoginRequest } from '../../../core/auth/auth.model';
 
 @Component({
-  selector: 'app-sign-in',
-  standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCardModule,
-    CommonModule
-  ],
-  templateUrl: './sign-in.component.html',
+    selector: 'app-sign-in',
+    standalone: true,
+    imports: [
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+        CommonModule
+    ],
+    templateUrl: './sign-in.component.html',
 })
 
-export class SignInComponent {
-  private readonly formBuilder = inject(FormBuilder);
-  private readonly authService = inject(AuthService);
+export class SignInComponent implements OnInit {
 
-  // Create SignInForm
-  signInForm = this.formBuilder.nonNullable.group({
-    email: [ '', [ Validators.required, Validators.email ]],
-    password: [ '', [ Validators.required, Validators.minLength(8) ]],
-  })
+    signInForm!: UntypedFormGroup;
 
-  /**
-   * Sign In
-   * @returns 
-   */
-  signIn(): void {
+    hidePassword = true;
 
-    if (this.signInForm.invalid) {
-      this.signInForm.markAllAsTouched();
-      return;
+    /**
+     * Constructor
+     */
+    constructor(
+        private _authService: AuthService,
+        private _formBuilder: UntypedFormBuilder, 
+    ){}
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    ngOnInit(): void {
+
+        // Create SignInForm
+        this.signInForm = this._formBuilder.group({
+            email: [ 'khairulizzatroslan@gmail.com', [ Validators.required, Validators.email ]],
+            password: [ '870327Tun@', [ Validators.required, Validators.minLength(8) ]],
+        })
     }
 
-    const request : LoginRequest = this.signInForm.getRawValue();
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
 
-    this.authService.signIn(request).subscribe({
-      next : response => {
-        console.log(response);
-      },
-      error : error => {
-        console.error(error);
-      }
-    });
+    /**
+     * Sign In & Get User by ID
+     * @returns 
+     */
+    signIn(): void {
 
-  }
+        // Check form validation
+        if (this.signInForm.invalid) {
+            this.signInForm.markAllAsTouched();
+            return;
+        }
+
+        // Assign body
+        const body : LoginRequest = this.signInForm.getRawValue();
+
+        this._authService.signIn(body).subscribe({
+            next : response => {
+                
+                // Save tokens
+                localStorage.setItem('accessToken', response.accessToken);
+
+                // Get user profile
+                this._authService.getUserDetails(response.userId).subscribe({
+                next: user => {
+                    console.log(user);
+                }
+                });
+            },
+            error : error => {
+                console.error(error);
+            }
+        });
+
+    }
 
 }
