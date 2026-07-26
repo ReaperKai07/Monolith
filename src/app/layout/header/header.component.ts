8aa95eeb2809d91b1bd3e4ed90cd47b8f5d7f498
Khairul Izzat Roslan <69512217+ReaperKai07@users.noreply.github.com>
2026-07-26 23:59:41 +0800

    update header UI and diplay user name

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { UserService } from '../../core/user/user.service';
 
 @Component({
     selector: 'app-header',
+    standalone: true,
     templateUrl: './header.component.html',
     imports: [
-      
+        AsyncPipe
     ],
 })
 
 export class HeaderComponent {
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Dependencies
+    // -----------------------------------------------------------------------------------------------------
+
+    private readonly _userService = inject(UserService);
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public properties
+    // -----------------------------------------------------------------------------------------------------
+
+    readonly currentUser$ = this._userService.currentUser$;
 
 }
