d5dfb27631a4cf4df2256ccbc037ed85b5d229df
Khairul Izzat Roslan <69512217+ReaperKai07@users.noreply.github.com>
2026-07-27 00:00:13 +0800

    restoring user state on app initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoadingOverlayComponent } from './shared/components/loading-overlay/loading-overlay.component';
+import { UserService } from './core/user/user.service';
+import { LoadingOverlayService } from './core/services/loading-overlay.service';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +14,38 @@ import { LoadingOverlayComponent } from './shared/components/loading-overlay/loa
   templateUrl: './app.component.html',
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(
-  ){}
+    // -----------------------------------------------------------------------------------------------------
+    // @ Dependencies
+    // -----------------------------------------------------------------------------------------------------
+
+    private readonly _userService = inject(UserService);
+    private readonly _loadingService = inject(LoadingOverlayService);
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    ngOnInit(): void {
+
+        // Show loading overlay
+        this._loadingService.show();
+
+        // Restore current user
+        this._userService
+            .initializeCurrentUser()
+            .pipe(
+                finalize(() => {
+                    // Hide loading overlay
+                    this._loadingService.hide();
+                })
+            )
+            .subscribe({
+                error: error => {
+                    console.error('Failed to restore current user', error);
+                }
+            });
+    }
   
 }
