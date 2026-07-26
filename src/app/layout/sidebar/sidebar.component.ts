6f38e550d70440986ea7d2dc678fd9ce9e86cc3f
Khairul Izzat Roslan <69512217+ReaperKai07@users.noreply.github.com>
2026-07-26 17:29:36 +0800

    update sidebar UI design and add sign-out

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,13 +1,70 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../core/auth/auth.service';
+
+// navigation item model
+interface NavigationItem {
+    title: string;
+    route: string;
+    icon: 'dashboard' | 'projects' | 'experiences' | 'skills' | 'contacts';
+}
 
 @Component({
-  selector: 'app-sidebar',
-  imports: [
-
-  ],
-  templateUrl: './sidebar.component.html',
+    selector: 'app-sidebar',
+    templateUrl: './sidebar.component.html',
+    imports: [
+        RouterLink,
+        RouterLinkActive,
+    ],
 })
 
 export class SidebarComponent {
+
+  /**
+   * Constructor
+   */
+  constructor(
+    private _authService: AuthService,
+    private _router: Router,
+  ) {}
+
+    // Define navigation items for sidebar
+    navigationItems: NavigationItem[] = [
+        {
+            title: 'Dashboard',
+            route: '/dashboard',
+            icon: 'dashboard'
+        },
+        {
+            title: 'Projects',
+            route: '/projects',
+            icon: 'projects'
+        },
+        {
+            title: 'Experiences',
+            route: '/experiences',
+            icon: 'experiences'
+        },
+        {
+            title: 'Skills',
+            route: '/skills',
+            icon: 'skills'
+        },
+        {
+            title: 'Contacts',
+            route: '/contacts',
+            icon: 'contacts'
+        }
+    ];
+
+    // Sign out method to handle user sign-out
+    signOut(): void {
+        this._authService.signOut().subscribe(() => {
+            // Delay 1 second to simulate BE, then navigate to sign-in
+            setTimeout(() => {
+                this._router.navigate(['/sign-in']);
+            }, 1000); 
+        });
+    }
 
 }
