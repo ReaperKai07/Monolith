403d4f090aff5768c438c0099694dc5e7aaaa31a
Khairul Izzat Roslan <69512217+ReaperKai07@users.noreply.github.com>
2026-07-26 12:47:16 +0800

    added experience component

---
+++ src/app/modules/admin/experiences/experiences.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiences',
+  templateUrl: './experiences.component.html',
+  imports: [
+
+  ],
+})
+
+export class ExperiencesComponent {
+
+}
---
+++ src/app/modules/admin/experiences/experiences.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ExperiencesComponent } from './experiences.component';
+
+export default [
+    {
+        path: '',
+        component: ExperiencesComponent,
+    },
+] as Routes;
