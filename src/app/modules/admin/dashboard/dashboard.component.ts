d324ec923d3951fc4a11fe7b3ffee80913981c18
Khairul Izzat Roslan <khairulizzatroslan@gmail.com>
2026-07-27 17:29:34 +0800

    updating dashbord - ongoing

--- src/app/modules/admin/dashboard/dashboard.component.ts
+++ src/app/modules/admin/dashboard/dashboard.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  imports: [
-    MatIconModule
-  ],
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    imports: [
+        MatIconModule
+    ],
 })
 
 export class DashboardComponent {
