6391c40fbe41808974683d8dda116b288a0c66ff
Khairul Izzat Roslan <khairulizzatroslan@gmail.com>
2026-07-27 10:46:10 +0800

    added cryptek-lab module for reusable UIs

---
+++ src/app/modules/admin/cryptek-lab/cryptek-lab.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cryptek-lab',
+  templateUrl: './cryptek-lab.component.html',
+  imports: [
+
+  ],
+})
+
+export class CryptekLabComponent {
+
+}
