70ffd552b4ecb24d53e7b936d829852fd38c8c3d
Khairul Izzat Roslan <khairulizzatroslan@gmail.com>
2026-07-27 10:45:42 +0800

    added certificates component

---
+++ src/app/modules/admin/certificates/certificates.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-certificates',
+    templateUrl: './certificates.component.html',
+    imports: [
+
+    ],
+})
+
+export class CertificatesComponent {
+
+}
---
+++ src/app/modules/admin/certificates/certificates.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { CertificatesComponent } from './certificates.component';
+
+export default [
+    {
+        path: '',
+        component: CertificatesComponent,
+    },
+] as Routes;
