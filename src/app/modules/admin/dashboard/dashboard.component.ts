310395b414242c0932edcd70a4eac16e03e44311
Khairul Izzat Roslan <khairulizzatroslan@gmail.com>
2026-07-28 13:00:54 +0800

    update basic dashboard UI undecorated

--- src/app/modules/admin/dashboard/dashboard.component.ts
+++ src/app/modules/admin/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     imports: [
-        MatIconModule
+        MatIconModule,
+        MatListModule
     ],
 })
 
