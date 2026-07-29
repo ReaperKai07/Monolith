6ab94893440aa197d631fab0685fee8b4bb8698f
Khairul Izzat Roslan <69512217+ReaperKai07@users.noreply.github.com>
2026-07-29 22:07:50 +0800

    fix main layout overflow with the sidebar

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -4,15 +4,24 @@ import { HeaderComponent } from "../header/header.component";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-main-layout',
-  imports: [
-    SidebarComponent, 
-    HeaderComponent, 
-    RouterOutlet,
-  ],
-  templateUrl: './main-layout.component.html',
+    selector: 'app-main-layout',
+    imports: [
+        SidebarComponent, 
+        HeaderComponent, 
+        RouterOutlet,
+    ],
+    templateUrl: './main-layout.component.html',
 })
 
 export class MainLayoutComponent {
 
+    sidebarOpen = false;
+
+    toggleSidebar(): void {
+        this.sidebarOpen = !this.sidebarOpen;
+    }
+
+    closeSidebar(): void {
+        this.sidebarOpen = false;
+    }
 }
