982972be4acb2b3697230eca24cfb9d10004b94c
Khairul Izzat Roslan <69512217+ReaperKai07@users.noreply.github.com>
2026-08-06 00:23:11 +0800

    Use hash routing for GitHub Pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,26 +1,31 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/auth/auth.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   
-  providers: [
+    providers: [
 
-    provideRouter(routes),
+        provideRouter(
+            routes,
+            withHashLocation(),
+        ),
 
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-   
-    provideHttpClient(
-      withInterceptors([
-        authInterceptor
-      ])
-    ),
+        provideZoneChangeDetection({ 
+            eventCoalescing: true 
+        }), 
+      
+        provideHttpClient(
+            withInterceptors([
+                authInterceptor
+            ])
+        ),
 
-    provideAnimationsAsync(),
+        provideAnimationsAsync(),
 
-  ]
+    ]
   
 };
